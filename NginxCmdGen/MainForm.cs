d1c646abd717925d38d79560d9077247778bb3ad
liuquanal <liuquanal@sina.com>
2019-08-28 23:51:46 +0800

    修改布局样式

--- NginxCmdGen/MainForm.cs
+++ NginxCmdGen/MainForm.cs
@@ -10,9 +10,9 @@ using System.IO;
 
 namespace NginxCmdGen
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
@@ -177,6 +177,51 @@ namespace NginxCmdGen
             {
                 MessageBox.Show("选择此选项，将只生成通用命令，不包含Nginx特有命令。", "温馨提示");
             }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
